5f9ba8d95c90f0edcf610e8453e07b764e4ef74c
Luis Macias Hau <luissmh150@gmail.com>
2022-01-30 00:29:28 -0600

    Luis Macias PRACTICA2

--- conversionbinario.dart
+++ conversionbinario.dart
@@ -8,14 +8,11 @@ void main() {
   numeroabinario = int.parse(numbinario ?? ''); 
   int valorfinalbinario = 0;
   int i = 1;
-
+  
   while (numeroabinario > 0) {
     valorfinalbinario = valorfinalbinario + (numeroabinario % 2) * i;
-
     numeroabinario = (numeroabinario / 2).floor();
-
     i = i * 10;
   }
-
   print("El numero binario del numero $numeroabinario es  $valorfinalbinario");
 }
--- salario.dart
+++ salario.dart
@@ -8,14 +8,30 @@ void main() {
   const salariohora = 120;
   const salarioExtra = 175;
   var salario;
-
-  salario = horas * salariohora;
+  var horasExtra;
+  var horasExtrafinal;
+  var salarioconhorasExtra;
+  salario = 40 * salariohora;
 
   if (horas < 27) {
-    print!(
+    salario = horas * salariohora;
+    print(
         "Las horas trabajadas no son suficientes, se le levantara un acta administrativa");
     print("cuando acumules 2 se le dara de baja");
     print("y su salario de esta semana es de $salario pesos");
   }
-    print("su salario es de $salario");
+  if (horas > 40) {
+    horasExtra = horas - 40;
+    horasExtrafinal = horasExtra * salarioExtra;
+    salarioconhorasExtra = salario + horasExtrafinal;
+    print(
+        "su salario esta semana es de $salarioconhorasExtra y sus horas extra trabajadas son $horasExtra");
+  }
+
+  if (horas > 27 && horas < 40) {
+    salario = horas * salariohora;
+    print("Usted no cuenta con horas extra esta semana");
+    print(
+        "Sus horas de trabajo estan en lo requerido, su salario es de $salario");
+  }
 }
